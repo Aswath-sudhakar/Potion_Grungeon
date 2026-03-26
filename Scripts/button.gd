87dab3128ee331ba719ad72f313d39a483ac91ab
schem119 <schem119@umn.edu>
2026-03-26 17:40:25 -0500

    buttons

--- Scripts/button.gd
+++ Scripts/button.gd
@@ -8,4 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if button_pressed:
+		get_tree().change_scene_to_file("res://Scenes/battle.tscn")
--- Scripts/play_button.gd
+++ Scripts/play_button.gd
@@ -9,3 +9,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	#add scene to switch to
+	if button_pressed:
+		get_tree().change_scene_to_file("res://Scenes/map.tscn")
